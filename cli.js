f423e289f14e7e1bf55414414365d55a246fb4d5
federico-ntr <federicontr@gmail.com>
2021-06-08 15:17:48 +0200

    New: help on '-h', '--help' or no args passed

--- cli.js
+++ cli.js
@@ -4,40 +4,50 @@ const args = process.argv.slice(2);
 const fs = require('fs');
 const { MongoClient } = require('mongodb');
 
-const json = args[0];
+switch (args[0]) {
+    case '-h':
+    case '--help':
+    case undefined:
+        console.log('\n\nRefer to https://github.com/holoitsme/mongo-json-setter/blob/main/README.md for instructions on how to use this utility.\n')
+        break;
+    default:
+        mongoSetup(args[0]);
+}
 
-fs.readFile(json, 'utf8', (err, data) => {
-    if(err) {
-        console.error(err);
-        return;
-    }
-
-    //Parse JSON
-    let config;
-    try {
-        config = JSON.parse(data);
-    } catch (e) {
-        if (e instanceof SyntaxError) console.error('The specified file is not a valid JSON file')
-        return;
-    }
-
-    const url = `mongodb://${config.host}:${config.port}/${config.db_name}`;
-
-    MongoClient.connect(url, {useUnifiedTopology: true}, (err, db) => {
-        if (err) throw err;
-        const dbo = db.db(config.db_name);
-        console.log("Database created");
-        for (const [i, collection] of config.collections.entries()) {
-            dbo.createCollection(collection.name, (err, coll) => {
-                if (err) throw err;
-                console.log("Collection created!");
-                coll.insertOne(collection.fields, (err, res) => {
-                    if (err) throw err;
-                    console.log("1 document inserted");
-                    if (i === config.collections.length - 1)
-                        db.close();
-                });
-            });
+function mongoSetup(json) {
+    fs.readFile(json, 'utf8', (err, data) => {
+        if(err) {
+            console.error(err);
+            return;
         }
+
+        //Parse JSON
+        let config;
+        try {
+            config = JSON.parse(data);
+        } catch (e) {
+            if (e instanceof SyntaxError) console.error('\n\nThe specified file is not a valid JSON file.\n')
+            return;
+        }
+
+        const url = `mongodb://${config.host}:${config.port}/${config.db_name}`;
+
+        MongoClient.connect(url, {useUnifiedTopology: true}, (err, db) => {
+            if (err) throw err;
+            const dbo = db.db(config.db_name);
+            console.log("Database created");
+            for (const [i, collection] of config.collections.entries()) {
+                dbo.createCollection(collection.name, (err, coll) => {
+                    if (err) throw err;
+                    console.log("Collection created!");
+                    coll.insertOne(collection.fields, (err, res) => {
+                        if (err) throw err;
+                        console.log("1 document inserted");
+                        if (i === config.collections.length - 1)
+                            db.close();
+                    });
+                });
+            }
+        });
     });
-});
+}
